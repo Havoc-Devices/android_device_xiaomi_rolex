49a598c79a37d75d2f0844b4fb03262162d26a2e
AryanPatidar <ap9977925180@gmail.com>
2018-07-09 08:52:09 +0000

    Havoc-ify

--- havoc.mk
+++ havoc.mk
@@ -19,8 +19,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc stuff
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit from rolex device
 $(call inherit-product, device/xiaomi/rolex/device.mk)
@@ -29,7 +29,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 4A
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := havoc_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+for var in eng user userdebug; do
+  add_lunch_combo havoc_rolex-$var
+done
